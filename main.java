1036f370aefe29307cf405cd461cf744195dcf8c
Reapper <axel@e-marceau.com>
2021-08-28 10:33:35 +0200

    boucle for ok

--- main.java
+++ main.java
@@ -12,33 +12,14 @@ public class main
         int varOdd = 1;
         int varPair = 0;
 
-        while(true)
+        for(;(Number >= varOdd) | (Number >= varPair);)
         {
-            //System.out.println("start"+ varOdd + " | " + varPair + " | " + Number);
-            if(Number == varOdd)
-            {
-                //System.out.println("odd");
-                Result = "Is Odd";
-                break;
-            } else {
-                //System.out.println("else 1"+ varOdd + " | " + varPair + " | " + Number);
-                if(Number == varPair)
-                {
-                    //System.out.println("pair");
-                    Result = "Is Pair";
-                    break;
-                } else {
-                    //System.out.println("else 2");
-                   // System.out.println("not " + varOdd + " | " + varPair + " | " + Number);
-                    Result = "Is Not";
-                    varOdd+=2;
-                    varPair+=2;
+            if(Number == varOdd) Result = "Is Odd";
+            if(Number == varPair) Result = "Is Pair";
 
-                }// END if(Number == varPair)
-
-            }// END if(Number == varOdd)
-
-        }// END while(Number != (varOdd | varPair))
+            varOdd+=2;
+            varPair+=2;            
+        }// END for(;(Number >= varOdd) | (Number >= varPair);)
         
         System.out.println(Result);
     }// END private static void OddOrPair(int Number)
